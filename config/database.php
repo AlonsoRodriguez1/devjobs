43d9c7be4d9aa730d357c7c8905df0904e16f616
unknown <fabianavilesfaar@gmail.com>
2022-09-26 19:57:51 -0600

    Deployment devjobs

--- config/database.php
+++ config/database.php
@@ -126,7 +126,7 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-            'unix_socket'   => env('UNIX_SOCKET',    '/Applications/MAMP/tmp/mysql/mysql.sock'),
+            
         ],
 
         'default' => [
